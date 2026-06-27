3d606631874a928f651669c47404bd147de20b27
s1f10260105 <s1f102601058@iniad.org>
2026-06-28 00:59:01 +0900

    concept関連編集
    conceptページにバーガー実装
    ヘッダー下CONCEPT>>表記はワイヤーフレーム内のページ移動を表すものだったため消去しました

---
+++ ja/concept.js
@@ -0,0 +1,17 @@
+const hamburger = document.querySelector('.hamburger-menu');
+const menu = document.querySelector('#global-menu');
+
+hamburger.addEventListener('click', () => {
+    menu.classList.toggle('is-active');
+
+    const expanded = hamburger.getAttribute('aria-expanded') === 'true';
+    hamburger.setAttribute('aria-expanded', !expanded);
+});
+
+document.querySelectorAll('.has-submenu > a').forEach(item => {
+    item.addEventListener('click', (e) => {
+        e.preventDefault();
+        const parent = item.parentElement;
+        parent.classList.toggle('open');
+    });
+});

bef29f7427f827205b8123ca5d5b9d07c1e9199d
s1f10260105 <s1f102601058@iniad.org>
2026-07-01 13:57:20 +0900

    Mountain/Ocean画像追加
    写真スライドショーwip

--- ja/index.js
+++ ja/index.js
@@ -16,11 +16,3 @@ document.querySelectorAll('.has-submenu > a').forEach(item => {
     });
 });
 
-let index = 0;
-const slides = document.querySelectorAll('.slide')
-function showNextSlide() {
-    slides[index].classList.remove('active');
-    index = (index + 1) % slides.length;
-    slides[index].classList.add('active');
-}
-setInterval(showNextSlide, 3000);
